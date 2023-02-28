30e8746a2763adf0cfda3ef37f53f575fa1d661a
LUCAS SILVA IRINEU <lucasirineu@hotmail.com>
2023-02-28 17:24:16 -0300

    Add files via upload

---
+++ pub-sub-with-zeromq-LucasSI-main/const.py
@@ -0,0 +1,5 @@
+CHAT_SERVER_HOST = "127.0.0.1"
+CHAT_SERVER_PORT = 5678
+
+# IDs de usuários registrados com os respectivos endereços IP e números de porta
+registry = {"Lucas":("127.0.0.1",5680), "Mateus":("127.0.0.1",5681)}
---
+++ pub-sub-with-zeromq-LucasSI-main/user.py
@@ -0,0 +1,13 @@
+import zmq, time
+from const_2 import *#-
+
+context = zmq.Context()
+s = context.socket(zmq.PUB)        # create a publisher socket
+p = "tcp://"+ HOST +":"+ PORT      # how and where to communicate
+s.bind(p)                          # bind socket to the address
+
+while True:
+        
+	time.sleep(5)                    # wait every 5 seconds
+	msg = str.encode("TIME " + time.asctime())
+	s.send(msg) # publish the current time
